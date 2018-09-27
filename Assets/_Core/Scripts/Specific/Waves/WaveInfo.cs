c5264ac78b44c3603de390fc81f1c2e0649bba95
Darkfafi <ik_ben_ramses@hotmail.com>
2018-09-27 20:15:53 +0200

    Made it so the WaveInfo class / respective structs and the Tracker are in a WaveSystemInternal namespace

--- Assets/_Core/Scripts/Specific/Waves/WaveInfo.cs
+++ Assets/_Core/Scripts/Specific/Waves/WaveInfo.cs
@@ -1,38 +1,41 @@
-﻿public class WaveInfo
+﻿namespace Game.WaveSystemInternal
 {
-    public int WaveIndex { get; private set; }
-    public WaveSectionObject[] WaveSections { get; private set; }
-
-    public WaveInfo(int waveIndex, params WaveSectionObject[] waveSections)
+    public class WaveInfo
     {
-        WaveIndex = waveIndex;
-        WaveSections = waveSections;
-    }
-}
+        public int WaveIndex { get; private set; }
+        public WaveSectionObject[] WaveSections { get; private set; }
 
-public struct WaveSectionObject
-{
-    public EnemySpawnData[] Enemies;
-    public float TimeToFightEnemiesInSeconds;
-}
-
-public struct EnemySpawnData
-{
-    public string EnemyWord { get; private set; }
-    public string[] EnemyNextWords { get; private set; }
-    public EnemyModel.EnemyCharacterType EnemyCharacterType { get; private set; }
-    public int Damage { get; private set; }
-
-    public EnemySpawnData(EnemyModel.EnemyCharacterType enemyType, int damage, string word, params string[] nextWords)
-    {
-        EnemyWord = word;
-        EnemyNextWords = nextWords;
-        EnemyCharacterType = enemyType;
-        Damage = damage;
+        public WaveInfo(int waveIndex, params WaveSectionObject[] waveSections)
+        {
+            WaveIndex = waveIndex;
+            WaveSections = waveSections;
+        }
     }
 
-    public EnemyModel CreateEnemy()
+    public struct WaveSectionObject
     {
-        return new EnemyModel(EnemyCharacterType, Damage, EnemyWord, EnemyNextWords);
+        public EnemySpawnData[] Enemies;
+        public float TimeToFightEnemiesInSeconds;
+    }
+
+    public struct EnemySpawnData
+    {
+        public string EnemyWord { get; private set; }
+        public string[] EnemyNextWords { get; private set; }
+        public EnemyModel.EnemyCharacterType EnemyCharacterType { get; private set; }
+        public int Damage { get; private set; }
+
+        public EnemySpawnData(EnemyModel.EnemyCharacterType enemyType, int damage, string word, params string[] nextWords)
+        {
+            EnemyWord = word;
+            EnemyNextWords = nextWords;
+            EnemyCharacterType = enemyType;
+            Damage = damage;
+        }
+
+        public EnemyModel CreateEnemy()
+        {
+            return new EnemyModel(EnemyCharacterType, Damage, EnemyWord, EnemyNextWords);
+        }
     }
 }
--- Assets/_Core/Scripts/Specific/Waves/WaveSystemModel.cs
+++ Assets/_Core/Scripts/Specific/Waves/WaveSystemModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using Game.WaveSystemInternal;
 
 public class WaveSystemModel : BaseModel
 {
