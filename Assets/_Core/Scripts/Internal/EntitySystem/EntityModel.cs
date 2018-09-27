043da7939dfba3f502d00e0fbbee9a7642079bc5
Darkfafi <ik_ben_ramses@hotmail.com>
2018-09-27 21:10:48 +0200

    Gave EntityTransform its own file

--- Assets/_Core/Scripts/Internal/EntitySystem/EntityModel.cs
+++ Assets/_Core/Scripts/Internal/EntitySystem/EntityModel.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-public class EntityModel : BaseModel
+﻿public class EntityModel : BaseModel
 {
     public EntityTransform Transform { get; protected set; }
 
@@ -8,43 +6,5 @@ public class EntityModel : BaseModel
     {
         Transform = new EntityTransform();
         EntityTracker.Instance.Register(this);
-    }
-}
-
-public class EntityTransform
-{
-    public Vector3 Position = Vector3.zero;
-    public Vector3 Rotation = Vector3.zero;
-    public Vector3 Scale = Vector3.one;
-
-
-    public void TranslatePosition(float x, float y, float z)
-    {
-        Vector3 p = Position;
-        p.x += x;
-        p.y += y;
-        p.z += z;
-        Position = p;
-    }
-
-    public void SetPosX(float x)
-    {
-        Vector3 p = Position;
-        p.x = x;
-        Position = p;
-    }
-
-    public void SetPosY(float y)
-    {
-        Vector3 p = Position;
-        p.y = y;
-        Position = p;
-    }
-
-    public void SetPosZ(float z)
-    {
-        Vector3 p = Position;
-        p.z = z;
-        Position = p;
     }
 }
---
+++ Assets/_Core/Scripts/Internal/EntitySystem/Parts/EntityTransform.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class EntityTransform
+{
+    public Vector3 Position = Vector3.zero;
+    public Vector3 Rotation = Vector3.zero;
+    public Vector3 Scale = Vector3.one;
+
+
+    public void TranslatePosition(float x, float y, float z)
+    {
+        Vector3 p = Position;
+        p.x += x;
+        p.y += y;
+        p.z += z;
+        Position = p;
+    }
+
+    public void SetPosX(float x)
+    {
+        Vector3 p = Position;
+        p.x = x;
+        Position = p;
+    }
+
+    public void SetPosY(float y)
+    {
+        Vector3 p = Position;
+        p.y = y;
+        Position = p;
+    }
+
+    public void SetPosZ(float z)
+    {
+        Vector3 p = Position;
+        p.z = z;
+        Position = p;
+    }
+}
