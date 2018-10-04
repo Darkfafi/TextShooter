4e67aa5604b9a381a17492d3da83b0d24fbc2b3f
Darkfafi <ik_ben_ramses@hotmail.com>
2018-10-04 22:00:20 +0200

    Working on UI elements / prefabs / overal setup

---
+++ Assets/_Core/Scripts/Specific/UI/WordsDisplayer/WordUIDisplayItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordUIDisplayItem : MonoBehaviour
+{
+    [SerializeField]
+    private Text _wordTextDisplay;
+	
+
+    public void SetText(string text)
+    {
+        _wordTextDisplay.text = text;
+    }
+}
---
+++ Assets/_Core/Scripts/Specific/UI/WordsDisplayer/WordsDisplayerView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordsDisplayerView : MonoBaseView
+{
+    [SerializeField]
+    private WordUIDisplayItem _wordUIDisplayItemPrefab;
+
+
+}
