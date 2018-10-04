67ee2659adbd255caf01deb1b17406b9483ce0b8
Darkfafi <ik_ben_ramses@hotmail.com>
2018-10-04 21:48:05 +0200

    Created component system (ModelComponents & BaseModelComponent)

---
+++ Assets/_Core/Scripts/Internal/MVC/Extra/Components/Core/BaseModelComponent.cs
@@ -0,0 +1,46 @@
+﻿public enum ModelComponentState
+{
+    None,
+    Initialized,
+    Active,
+    Removed
+}
+
+public abstract class BaseModelComponent
+{
+    public ModelComponents Components { get; private set; }
+    public ModelComponentState ComponentState { get; private set; }
+
+    public void Initialize(ModelComponents parent)
+    {
+        if (ComponentState != ModelComponentState.None)
+            return;
+        
+        Components = parent;
+        ComponentState = ModelComponentState.Initialized;
+        Added();
+    }
+
+    public void SignalReady()
+    {
+        if(ComponentState != ModelComponentState.Initialized)
+            return;
+        
+        ComponentState = ModelComponentState.Active;
+        Ready();
+    }
+
+    public void Deinitialize()
+    {
+        if (ComponentState == ModelComponentState.Removed)
+            return;
+        
+        ComponentState = ModelComponentState.Removed;
+        Removed();
+        Components = null;
+    }
+
+    protected virtual void Added() { }
+    protected virtual void Ready() { }
+    protected virtual void Removed() { }
+}
