eb4965863eb149bffafa4e3101dc3d0be8f9d933
Darkfafi <ik_ben_ramses@hotmail.com>
2018-12-06 15:05:00 +0100

    Cleaning wave system model after destroy

--- Assets/_Core/Scripts/Specific/Waves/WaveSystemView.cs
+++ Assets/_Core/Scripts/Specific/Waves/WaveSystemView.cs
@@ -18,6 +18,7 @@ public class WaveSystemView : MonoBaseView
 	protected override void OnViewDestroy()
 	{
 		_waveSystemModel.SpawnEnemyEvent -= OnSpawnEnemyEvent;
+		_waveSystemModel = null;
 	}
 
 	#endregion
