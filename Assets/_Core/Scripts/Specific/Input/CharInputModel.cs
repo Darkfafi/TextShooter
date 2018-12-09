2177ad833724c90770b6d6bc063d66ffa618532c
Darkfafi <ik_ben_ramses@hotmail.com>
2018-12-09 17:21:54 +0100

    Made char input with a keyboard view.

---
+++ Assets/_Core/Scripts/Specific/Input/CharInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class CharInputModel : BaseModel
+{
+	public event Action<char> InputEvent;
+
+	public void DoCharInput(char charInput)
+	{
+		if(InputEvent != null)
+		{
+			InputEvent(charInput);
+		}
+	}
+}
