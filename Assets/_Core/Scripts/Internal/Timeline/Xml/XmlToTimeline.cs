24edcac089483ffe13762881b6a1e5809f3c592c
Darkfafi <ik_ben_ramses@hotmail.com>
2019-01-10 20:11:37 +0100

    Made it so the parsing to a TimelineEventSlot is in a separate method. Made all the parsing methods public.

--- Assets/_Core/Scripts/Internal/Timeline/Xml/XmlToTimeline.cs
+++ Assets/_Core/Scripts/Internal/Timeline/Xml/XmlToTimeline.cs
@@ -20,78 +20,22 @@ public static class XmlToTimeline
 			if(node.Name == TimelineInternalGlobals.NODE_EVENT)
 			{
 				TimelineEventSlot<T> timelineEventSlot = null;
-
-				foreach(XmlNode innerEventNode in node)
+				try
 				{
-					if(innerEventNode.Name == TimelineInternalGlobals.NODE_EVENT_DEFAULT)
+					timelineEventSlot = ParseEventNodeToTimelineSlot<T>(node, getDataParserForTypeMethod);
+					if(timelineEventSlot != null)
 					{
-						if(timelineEventSlot == null)
-						{
-							try
-							{
-								timelineEventSlot = new TimelineEventSlot<T>(ParseNodeToPotentialEvent(innerEventNode, getDataParserForTypeMethod));
-							}
-							catch(Exception e)
-							{
-								throw new Exception(e.Message + ". Event number " + eventNumber);
-							}
-						}
-						else
-						{
-							throw new Exception("Event may only have 1 default node. Event Number " + eventNumber);
-						}
+						timeline.EnqueueTimelineSlot(timelineEventSlot);
+						eventNumber++;
 					}
-					else if(innerEventNode.Name == TimelineInternalGlobals.NODE_EVENT_CONDITION)
+					else
 					{
-						if(timelineEventSlot != null)
-						{
-							List<KeyValuePair<string, bool>> conditions = new List<KeyValuePair<string, bool>>();
-							foreach(XmlNode innerConditionEvent in innerEventNode)
-							{
-								if(innerConditionEvent.Name == TimelineInternalGlobals.NODE_EVENT_CONDITION_KEY)
-								{
-									bool condition;
-									XmlNode valueNode = innerConditionEvent.Attributes[TimelineInternalGlobals.ATTRIBUTE_EVENT_CONDITION_KEY_VALUE];
-									string valueString = valueNode == null ? "true" : valueNode.InnerText;
-									if(!bool.TryParse(valueString, out condition))
-									{
-										condition = true;
-									}
-
-									conditions.Add(new KeyValuePair<string, bool>(innerConditionEvent.InnerText, condition));
-								}
-							}
-							if(conditions.Count > 0)
-							{
-								try
-								{
-									timelineEventSlot.AddConditionalEvent(ParseNodeToPotentialEvent(innerEventNode, getDataParserForTypeMethod), conditions.ToArray());
-								}
-								catch(Exception e)
-								{
-									throw new Exception(e.Message + ". Event number " + eventNumber);
-								}
-							}
-							else
-							{
-								throw new Exception("Condition Event given without conditionKey nodes. Event number " + eventNumber);
-							}
-						}
-						else
-						{
-							throw new Exception("Condition Event came before Default event. Event number " + eventNumber);
-						}
+						throw new Exception("TimelineEventSlot was null. Event number " + eventNumber);
 					}
 				}
-
-				if(timelineEventSlot != null)
+				catch(Exception e)
 				{
-					timeline.EnqueueTimelineSlot(timelineEventSlot);
-					eventNumber++;
-				}
-				else
-				{
-					throw new Exception("Timeline could not add event due to it not having a default nor condition tags. Event number " + eventNumber);
+					throw new Exception(e.Message + ". Event number " + eventNumber);
 				}
 			}
 		}
@@ -99,7 +43,70 @@ public static class XmlToTimeline
 		return timeline;
 	}
 
-	private static PotentialEvent ParseNodeToPotentialEvent(XmlNode eventCaseNode, Func<string, BaseTimelineEventDataParser> getDataParserForTypeMethod)
+	public static TimelineEventSlot<T>ParseEventNodeToTimelineSlot<T>(XmlNode eventNode, Func<string, BaseTimelineEventDataParser> getDataParserForTypeMethod) where T : class, IGame
+	{
+		TimelineEventSlot<T> timelineEventSlot = null;
+
+		foreach(XmlNode innerEventNode in eventNode)
+		{
+			if(innerEventNode.Name == TimelineInternalGlobals.NODE_EVENT_DEFAULT)
+			{
+				if(timelineEventSlot == null)
+				{
+					timelineEventSlot = new TimelineEventSlot<T>(ParseNodeToPotentialEvent(innerEventNode, getDataParserForTypeMethod));
+				}
+				else
+				{
+					throw new Exception("Event may only have 1 default node.");
+				}
+			}
+			else if(innerEventNode.Name == TimelineInternalGlobals.NODE_EVENT_CONDITION)
+			{
+				if(timelineEventSlot != null)
+				{
+					List<KeyValuePair<string, bool>> conditions = new List<KeyValuePair<string, bool>>();
+					foreach(XmlNode innerConditionEvent in innerEventNode)
+					{
+						if(innerConditionEvent.Name == TimelineInternalGlobals.NODE_EVENT_CONDITION_KEY)
+						{
+							bool condition;
+							XmlNode valueNode = innerConditionEvent.Attributes[TimelineInternalGlobals.ATTRIBUTE_EVENT_CONDITION_KEY_VALUE];
+							string valueString = valueNode == null ? "true" : valueNode.InnerText;
+							if(!bool.TryParse(valueString, out condition))
+							{
+								condition = true;
+							}
+
+							conditions.Add(new KeyValuePair<string, bool>(innerConditionEvent.InnerText, condition));
+						}
+					}
+					if(conditions.Count > 0)
+					{
+						timelineEventSlot.AddConditionalEvent(ParseNodeToPotentialEvent(innerEventNode, getDataParserForTypeMethod), conditions.ToArray());
+					}
+					else
+					{
+						throw new Exception("Condition Event given without conditionKey nodes.");
+					}
+				}
+				else
+				{
+					throw new Exception("Condition Event came before Default event.");
+				}
+			}
+		}
+
+		if(timelineEventSlot != null)
+		{
+			return timelineEventSlot;
+		}
+		else
+		{
+			throw new Exception("Timeline could not add event due to it not having a default nor condition tags.");
+		}
+	}
+
+	public static PotentialEvent ParseNodeToPotentialEvent(XmlNode eventCaseNode, Func<string, BaseTimelineEventDataParser> getDataParserForTypeMethod)
 	{
 		BaseTimelineEventDataParser dataParser = null;
 		XmlNode xmlData = null;
