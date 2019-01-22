dfea1c4f64359a7ed62d617b162301f121bd6252
Darkfafi <ik_ben_ramses@hotmail.com>
2019-01-22 22:30:41 +0100

    Made an editor for both the ModelTags component as the ModelTransform component

---
+++ Assets/_Core/Scripts/Internal/MVC/Extra/Components/Extra/Editor/ModelTagsEditor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ModelComponentEditor(typeof(ModelTags))]
+public class ModelTagsEditor : BaseModelComponentEditor
+{
+	private string _currentTagAddString = "";
+
+	public override void OnGUI(BaseModelComponent component)
+	{
+		ModelTags tagsComponent = component as ModelTags;
+
+		string[] tags = tagsComponent.GetTags();
+		EditorGUILayout.BeginHorizontal();
+		_currentTagAddString = EditorGUILayout.TextField("Add Tag: ", _currentTagAddString);
+		if(GUILayout.Button("+", GUILayout.Width(40)))
+		{
+			tagsComponent.AddTag(_currentTagAddString);
+			_currentTagAddString = string.Empty;
+		}
+		EditorGUILayout.EndHorizontal();
+
+		for(int i = 0; i < tags.Length; i++)
+		{
+			EditorGUILayout.BeginHorizontal();
+			GUIStyle s = new GUIStyle(GUI.skin.label);
+			s.normal.textColor = new Color(0.5f, 0.3f, 0.6f);
+			GUILayout.Label(" * " + tags[i], s);
+			s = new GUIStyle(GUI.skin.button);
+			s.normal.textColor = Color.red;
+			if(GUILayout.Button("x", s, GUILayout.Width(25)))
+			{
+				tagsComponent.RemoveTag(tags[i]);
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+
+		EditorGUILayout.Space();
+		
+	}
+}
---
+++ Assets/_Core/Scripts/Internal/MVC/Extra/Components/Extra/Editor/ModelTransformEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+[ModelComponentEditor(typeof(ModelTransform))]
+public class ModelTransformEditor : BaseModelComponentEditor
+{
+	public override void OnGUI(BaseModelComponent transformComponent)
+	{
+		ModelTransform t = transformComponent as ModelTransform;
+
+		t.Position = EditorGUILayout.Vector3Field("Position", t.Position);
+		t.Rotation = EditorGUILayout.Vector3Field("Rotation", t.Rotation);
+		t.Scale = EditorGUILayout.Vector3Field("Scale", t.Scale);
+	}
+}
