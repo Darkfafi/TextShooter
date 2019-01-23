60bab43c9a1a127c2147a743c4d7af1312ded7af
Darkfafi <ik_ben_ramses@hotmail.com>
2019-01-23 12:09:24 +0100

    Made it so you can now search tags through the project to add them. (also, the typing of tags is still supported)

--- Assets/_Core/Scripts/Internal/MVC/Extra/Components/Extra/Editor/ModelTagsEditor.cs
+++ Assets/_Core/Scripts/Internal/MVC/Extra/Components/Extra/Editor/ModelTagsEditor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections;
+using System.Linq;
+using System.Reflection;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -8,6 +9,7 @@ using UnityEngine;
 public class ModelTagsEditor : BaseModelComponentEditor
 {
 	private string _currentTagAddString = "";
+	private SearchWindow _openSearchWindow = null;
 
 	public override void OnGUI(BaseModelComponent component)
 	{
@@ -16,11 +18,49 @@ public class ModelTagsEditor : BaseModelComponentEditor
 		string[] tags = tagsComponent.GetTags();
 		EditorGUILayout.BeginHorizontal();
 		_currentTagAddString = EditorGUILayout.TextField("Add Tag: ", _currentTagAddString);
-		if(GUILayout.Button("+", GUILayout.Width(40)))
+		if(GUILayout.Button("+", GUILayout.Width(30)))
 		{
 			tagsComponent.AddTag(_currentTagAddString);
 			_currentTagAddString = string.Empty;
 		}
+
+		if(GUILayout.Button("search"))
+		{
+			Type[] tagHolders = Assembly.GetAssembly(typeof(ModelTags)).GetTypes().Where(t => t.GetCustomAttributes(typeof(TagsHolderAttribute), true).Length > 0 && t.IsClass).ToArray();
+			List<string> allTags = new List<string>();
+			foreach(Type tagsHolder in tagHolders)
+			{
+				FieldInfo[] fields = tagsHolder.GetFields(BindingFlags.Public | BindingFlags.Static).Where(t => t.GetCustomAttributes(typeof(TagFieldAttribute), true).Length > 0 && t.IsLiteral && !t.IsInitOnly).ToArray();
+				foreach(FieldInfo f in fields)
+				{
+					object v = f.GetValue(null);
+					if(v != null)
+					{
+						if(tagsComponent != null && tagsComponent.HasTag(v.ToString()))
+							continue;
+
+						allTags.Add(v.ToString());
+					}
+				}
+			}
+			_openSearchWindow = SearchWindow.OpenWindow((index) =>
+			{
+				if(index >= 0)
+				{
+					if(tagsComponent != null)
+					{
+						tagsComponent.AddTag(allTags[index]);
+					}
+
+					if(_openSearchWindow != null)
+					{
+						_openSearchWindow.Close();
+						_openSearchWindow = null;
+					}
+				}
+			}, allTags.ToArray());
+		}
+
 		EditorGUILayout.EndHorizontal();
 
 		for(int i = 0; i < tags.Length; i++)
@@ -40,5 +80,14 @@ public class ModelTagsEditor : BaseModelComponentEditor
 
 		EditorGUILayout.Space();
 		
+	}
+
+	public override void OnClose()
+	{
+		if(_openSearchWindow != null)
+		{
+			_openSearchWindow.Close();
+			_openSearchWindow = null;
+		}
 	}
 }
