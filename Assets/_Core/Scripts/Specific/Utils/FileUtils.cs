62b86897e9575560d35d39593930e2e68e774964
Darkfafi <ik_ben_ramses@hotmail.com>
2019-01-30 17:42:42 +0100

    Made FileUtils with a easy path method for simple use.

---
+++ Assets/_Core/Scripts/Specific/Utils/FileUtils.cs
@@ -0,0 +1,12 @@
+﻿public static class FileUtils
+{
+	public static string PathToFile(string fileName, params string[] pathParts)
+	{
+		string s = "";
+		for(int i = 0; i < pathParts.Length; i++)
+		{
+			s += pathParts[i] + "/";
+		}
+		return s + fileName;
+	}
+}
