810cb09a4c68c6ad9732cbc512cd75bb8ac1192d
Darkfafi <ik_ben_ramses@hotmail.com>
2019-01-31 10:33:57 +0100

    Made BaseCooldownWeapon which is a BaseWeapon but with a cooldown mechanic.

---
+++ Assets/_Core/Scripts/Specific/Mechanic/LifeAndDamage/Weapons/BaseCooldownWeapon.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseCooldownWeapon : BaseWeapon
+{
+	public override bool CanBeUsed
+	{
+		get
+		{
+			return _cooldownProcess >= Cooldown;
+		}
+	}
+
+	public float Cooldown
+	{
+		get; private set;
+	}
+
+	private float _cooldownProcess;
+	private TimekeeperModel _timekeeper;
+
+	public void SetCooldown(float newValue)
+	{
+		Cooldown = newValue;
+	}
+
+	public void SetupCooldown(float cooldown, TimekeeperModel timekeeper)
+	{
+		if(_timekeeper != null)
+			return;
+
+		_timekeeper = timekeeper;
+		SetCooldown(cooldown);
+		_timekeeper.ListenToFrameTick(OnUpdate);
+	}
+
+	protected override bool OnUse(Lives livesComponent)
+	{
+		_cooldownProcess = 0f;
+		return DoUseLogics(livesComponent);
+	}
+
+	protected override void Removed()
+	{
+		if(_timekeeper != null)
+		{
+			_timekeeper.UnlistenFromFrameTick(OnUpdate);
+			_timekeeper = null;
+		}
+	}
+
+	protected abstract bool DoUseLogics(Lives livesComponent);
+
+	private void OnUpdate(float deltaTime, float timeScale)
+	{
+		if(Cooldown > _cooldownProcess)
+		{
+			_cooldownProcess += deltaTime * timeScale;
+		}
+	}
+}
